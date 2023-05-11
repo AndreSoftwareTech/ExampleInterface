63d14c15e2d3e9b58b7f496bb7305766cca60d7b
AndreRovitex <andrerovitex@gmail.com>
2023-05-11 09:01:32 -0300

    Subindo exemplo de Interfaces simplificada

---
+++ ExampleInterface/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp1.Interfaces;
+using ConsoleApp1.model;
+
+namespace teste
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Sampleclass sample = new Sampleclass();
+
+            IControl control = sample;
+            Isurface surface = sample;
+
+
+            sample.Nome = "Andre";
+            sample.sobrenome = "Vitor";
+            sample.cpf = "123456";
+            sample.idade = "20";
+
+            surface.paint(sample.Nome, sample.sobrenome);
+            control.paint(sample.cpf, sample.idade);
+        }
+    }
+}
---
+++ ExampleInterface/model/Sampleclass.cs
@@ -0,0 +1,26 @@
+﻿
+using ConsoleApp1.Interfaces;
+
+namespace ConsoleApp1.model
+{
+    internal class Sampleclass: IControl, Isurface
+    {
+        public string Nome { get; set; }
+        public string sobrenome { get; set; }
+        public string cpf { get; set; }
+        public string idade { get; set; }
+
+
+        void IControl.paint(string cpf, string idade)
+        {
+            Console.WriteLine($"CPF {cpf} IDADE {idade}");
+        }
+
+        void Isurface.paint(string Nome, string sobrenome)
+        {
+            Console.WriteLine($"NOME {Nome} SobreNome {sobrenome}");
+
+        }
+    }
+}
+
